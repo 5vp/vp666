027259a742f623230468744fef6b35468b8f586b
wvp <328582463@qq.com>
2023-07-06 15:45:35 +0800

    1.项目配置 2.增加跳过开屏logo

---
+++ UnityPJ/VPWebCommonPJ/Assets/Plugins/StopVray.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Scripting;
+
+[Preserve]
+public class StopVray
+{
+    #if UNITY_WEBGL
+    private static void Application_focusChanged(bool obj)
+    {
+        Application.focusChanged -= Application_focusChanged;
+        SplashScreen.Stop(SplashScreen.StopBehavior.StopImmediate);
+    }
+    #endif
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+    private static void BeforeSplashScreen()
+    {
+#if UNITY_WEBGL
+        Application.focusChanged += Application_focusChanged;
+#else
+        Task.Run(()=>{
+            SplashScreen.Stop(SplashScreen.StopBehavior.StopImmediate);
+        });
+#endif
+    }
+}
